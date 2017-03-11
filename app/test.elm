54a93713a6fe0d1613312ab18ac0d008938b20aa
Jasper Woudenberg <mail@jasperwoudenberg.com>
2017-03-11 12:35:40 +0100

    Remove test file.

--- app/test.elm
+++
@@ -1,8 +0,0 @@
-module Bla
-
-import Foo
--- I can just write stuff here without adverse effect
-import Bar as Klarinet exposing (things , Type)
-import Test.Something exposing (some, things)
-
-foo : Bar
